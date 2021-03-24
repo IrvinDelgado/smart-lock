1c14aa2170e4c48e2fe2e1a3e7a4462c1f52f2b6
IrvinDelgado <Idelgadoramirez@hawk.iit.edu>
2021-03-24 13:39:00 -0500

    added listener to sim

--- simulator/LockSim.py
+++ simulator/LockSim.py
@@ -9,15 +9,21 @@
 '''
 #%%
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import firebase_admin,credentials, firestore
+import threading
+import time
 
 # initialize sdk if needed
 if not firebase_admin._apps:
     cred = credentials.Certificate("../.secrets/smart-lock-admin_sdk.json") 
     default_app = firebase_admin.initialize_app(cred)
+    
 # initialize firestore instance
 firestore_db = firestore.client()
 doc_ref = firestore_db.document('locks/front-door')
+
+# Create an Event for notifying main thread.
+callback_done = threading.Event()
 
 def lock():
     doc_ref.update({'status':1,})
@@ -30,6 +36,17 @@ def toggleLock():
     else:
         unlock()
 
-# %%
-toggleLock()
-# %%
+# Create a callback on_snapshot function to capture changes
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        print(f'Received document snapshot: {doc.id}')
+        print(f'Doc: {doc.to_dict()}')
+    callback_done.set()
+
+
+# Watch the document
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+# Keep the app running
+while True:
+    time.sleep(100)
+
